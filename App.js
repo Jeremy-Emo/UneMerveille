ac769ad08cea1af9c2d029d1420864e36b6c8bec
redcreep76 <lucien.brouillaud@wanadoo.fr>
2018-09-06 11:39:10 +0200

    1.2.0 is coming

--- App.js
+++ App.js
@@ -4,98 +4,14 @@ import Arme from "./components/Arme";
 import InfosPlayer from "./components/InfosPlayer";
 import Header from "./components/Header";
 import {styles} from "./styles/styles";
+import {confWeapons} from "./config/weapons";
 
 export default class App extends React.Component {
 
   constructor(){
     super();
     this.state = {
-      weapons : [
-        {
-          id : 1,
-          nom : "Pierre",
-          bat : [3,5,10],
-          lvl : 1,
-          img : require("./images/pierre.png"),
-        },
-        {
-          id : 2,
-          nom : "Papier",
-          bat : [1,4],
-          lvl : 1,
-          img : require("./images/papier.png"),
-        },
-        {
-          id : 3,
-          nom : "Ciseaux",
-          bat : [2,6,9],
-          lvl : 1,
-          img : require("./images/ciseaux.png"),
-        },
-        {
-          id : 4,
-          nom : "Puit",
-          bat : [1,3,8,10,12],
-          lvl : 2,
-          img : require("./images/puit.png"),
-        },
-        {
-          id : 5,
-          nom : "Bazooka",
-          bat : [2,3,4,12],
-          lvl : 3,
-          img : require("./images/bazooka.png"),
-        },
-        {
-          id : 6,
-          nom : "Licorne",
-          bat : [2,5,8],
-          lvl : 4,
-          img : require("./images/licorne.png"),
-        },
-        {
-          id : 7,
-          nom : "The ULTIMATE",
-          bat : [1,2,3,4,5,7,9,12],
-          lvl : 20,
-          img : require("./images/ultimate.png"),
-        },
-        {
-          id : 8,
-          nom : "Spartiate",
-          bat : [1,2,3,5],
-          lvl : 6,
-          img : require("./images/spartiate.png"),
-        },
-        {
-          id : 9,
-          nom : "Corde",
-          bat : [1,4,6],
-          lvl : 3,
-          img : require("./images/corde.png"),
-        },
-        {
-          id : 10,
-          nom : "Bière",
-          bat : [7,8,11],
-          lvl : 6,
-          img : require("./images/biere.png"),
-        },
-        {
-          id : 11,
-          nom : "Arc-en-ciel",
-          bat : [6,7],
-          lvl : 7,
-          img : require("./images/rainbow.png"),
-        },
-        {
-          id : 12,
-          nom : "Poulet",
-          bat : [8,9],
-          lvl : 7,
-          img : require("./images/rainbow.png"),
-        },
-      ],
+      weapons : confWeapons,
       playerInfos : {
         xp : 0,
         money : 0,
@@ -143,7 +59,6 @@ export default class App extends React.Component {
           Animated.timing(this.state.rotationValue, {toValue: 180, duration: 800}).start(
             () => {
               if( playerWeapon.bat.indexOf(enemyWeapon.id) != -1 ){
-                console.log('gagné ! Vous : ' + playerWeapon.nom + ' ; Lui : ' + enemyWeapon.nom);
                 let exp = (this.state.playerInfos.xp + 1)
                 let pognon = (this.state.playerInfos.money + 5)
                 this.setState({
@@ -155,12 +70,10 @@ export default class App extends React.Component {
                 });
                 let test = AsyncStorage.setItem('playerInfos', JSON.stringify(this.state.playerInfos));
               } else if(enemyWeapon.bat.indexOf(playerWeapon.id) != -1){
-                console.log('perdu ! Vous : ' + playerWeapon.nom + ' ; Lui : ' + enemyWeapon.nom);
                 this.setState({
                   message: "Défaite !",
                 })
               } else {
-                console.log('egalité...  Vous : ' + playerWeapon.nom + ' ; Lui : ' + enemyWeapon.nom);
                 this.setState({
                   message: "Egalité...",
                 })
